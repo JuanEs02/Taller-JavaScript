60a43f17d170c854d0e8889d2e7581f22faed3c5
JuanEs02 <114095229+JuanEs02@users.noreply.github.com>
2022-10-15 14:09:06 -0500

    commit 02

--- script.js
+++ script.js
@@ -32,3 +32,43 @@ function Autos() {
     document.writeln("<br>" + "Azules: " + contador5 + "\n");
     document.writeln("<br>" + "Total de autos: " + contador);
 }
+function zoo() {
+
+    let n;
+    let total = 0;
+    let edad = 0;
+    let categoria1 = 0;
+    let categoria2 = 0;
+    let categoria3 = 0;
+    let animal;
+
+    n = parseInt(prompt("Elige 1-3 según el tipo de animal" + "\n1- Elefantes" + "\n2- Jirafas" + "\n3- Chimpances"))
+
+    if (n > 0 && n < 4) {
+        if (n == 1) {
+            animal = "Elefantes";
+            total = 20;
+        } else if (n == 2) {
+            animal = "Jirafas";
+            total = 15;
+        } else {
+            animal = "Chimpances";
+            total = 40;
+        }
+        for (let x = 1; x <= total; x++) {
+            edad = parseInt(prompt("Ingrese la edad"))
+            if (edad >= 0 && edad <= 1) {
+                categoria1++;
+            } else if (edad < 3) {
+                categoria2++;
+            } else {
+                categoria3++;
+            }
+        }
+    }
+
+    document.write("Porcentaje de edades de " + animal);
+    document.writeln("<br>" + (categoria1 / total) * 100 + "% de 0 a 1 año\n");
+    document.writeln("<br>" + (categoria2 / total) * 100 + "% de mas de 1 año y menos de 3");
+    document.writeln("<br>" + (categoria3 / total) * 100 + "% de 3 año o más");
+}
